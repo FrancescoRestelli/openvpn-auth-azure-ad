ca128fe576652b9b9127a3e5902f4cd14d75e1aa
FrancescoRestelli <FrancescoRestelli@users.noreply.github.com>
2021-10-28 10:52:00 +0200

    add eth0 subnet to client route

--- silent-install.sh
+++ silent-install.sh
@@ -1,4 +1,5 @@
-
+IP=$(ifconfig eth0 |grep -v inet6|grep inet|awk '{ print $2 }')
+MASK=$(ifconfig eth0 |grep -v inet6|grep inet|awk '{ print $4 }')
 export AUTO_INSTALL=y 
 export PORT_CHOICE=1 
 export PROTOCOL_CHOICE=1
@@ -6,7 +7,7 @@ export DNS=1
 export CLIENT=$1
 export OPENVPNDEB="openvpn_2.4.9-2ubuntu1_amd64.deb"
 export DISABLEREDIRECT=1
-export CLIENTROUTE="push \"route 168.63.129.16 255.255.255.255\""
+export CLIENTROUTE="push \"route ${IP::-1}0 $MASK\"\n"
 export NATHACK=1
 ./openvpn-install.sh
 
