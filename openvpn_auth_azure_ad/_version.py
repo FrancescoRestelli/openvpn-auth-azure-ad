f6b1c4181b786a3ddfc5da265ff950c4b11c9c56
FrancescoRestelli <FrancescoRestelli@users.noreply.github.com>
2022-11-16 20:04:58 +0100

    Update _version.py

--- openvpn_auth_azure_ad/_version.py
+++ openvpn_auth_azure_ad/_version.py
@@ -1,4 +1,8 @@
-from importlib.metadata import version, PackageNotFoundError
+import sys
+if sys.version_info >= (3, 8):
+    from importlib import version, PackageNotFoundError
+else:
+    from importlib_metadata import version, PackageNotFoundError
 
 try:
     __version__ = version("openvpn-auth-azure-ad")
