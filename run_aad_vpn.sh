c1095b8efc21a2531ff9e51ae86ca53a819d6449
FrancescoRestelli <FrancescoRestelli@users.noreply.github.com>
2022-11-16 20:15:31 +0100

    Update run_aad_vpn.sh

--- run_aad_vpn.sh
+++ run_aad_vpn.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-AAD_TOKEN_AUTHORITY="https://login.microsoftonline.com/$(cat /etc/azure_tenantid)" AAD_CLIENT_ID="$(cat /etc/azure_clientid)" /usr/bin/python3 /usr/local/openvpn-auth-azure-ad/openvpn-auth-azure-ad.py --auth-token --remember-user -s /dev/openvpn 
+AAD_TOKEN_AUTHORITY="https://login.microsoftonline.com/$(cat /etc/azure_tenantid)" AAD_CLIENT_ID="$(cat /etc/azure_clientid)" /usr/bin/python3 /usr/local/src/openvpn-auth-azure-ad/openvpn-auth-azure-ad.py --auth-token --remember-user -s /dev/openvpn 
